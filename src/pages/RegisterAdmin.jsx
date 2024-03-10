c66f0e47d6dd759f2b256fe394b67b369f36efd0
David <daviddwiyanto79@gmail.com>
2024-03-10 09:08:42 +0700

    fix(image)

--- src/pages/RegisterAdmin.jsx
+++ src/pages/RegisterAdmin.jsx
@@ -177,7 +177,7 @@ const RegisterAdmin = () => {
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
       <img
         className="mx-auto h-10 w-auto"
-        src="https://images.unsplash.com/photo-1582845512747-e42001c95638?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+        src="https://images.glints.com/unsafe/glints-dashboard.s3.amazonaws.com/company-logo/69d10eaa19dffcd1db7b51c2c6d8c039.png"
         alt="Your Company"
       />
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
